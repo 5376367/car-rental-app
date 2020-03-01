2ab4c2367d7beae2806cab5a9e539cbac8a6265b
5376367 <eliandrivka@gmail.com>
2020-03-01 13:07:39 +0200

    Add files via upload

--- Controllers/RentalsController.cs
+++ Controllers/RentalsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -11,11 +13,31 @@ using theking.Models.ViewModels;
 
 namespace theking.Controllers
 {
+   
     public class RentalsController : Controller
     {
         private CarRentalDBEntities db = new CarRentalDBEntities();
+        
+        // 3rd party API for sending emails
+        public static IRestResponse SendEmail(string address, string subject, string body)
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+            client.Authenticator =
+               new HttpBasicAuthenticator("api",
+                               "e9fa19a3d17ba1ab5b99c0088732b62e-9dda225e-a1691ce8");
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", "sandbox73aac42755ff45278fb5b15452375562.mailgun.org", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "The King Car Rental <postmaster@sandbox73aac42755ff45278fb5b15452375562.mailgun.org>");
+            request.AddParameter("to", address);
+            request.AddParameter("subject", subject);
+            request.AddParameter("text", body);
+            request.Method = Method.POST;
+            return client.Execute(request);
 
-     
+        }
+
 
         public ActionResult Index()
         {
@@ -148,7 +170,8 @@ namespace theking.Controllers
                                where pri.Class == car.Class
                                select pri).FirstOrDefault();
                 //the following line will return the wrong info if car was returned different year to when it was taken
-                int daysExpected = (rental.DateIn.Value.DayOfYear - rental.DateOut.Value.DayOfYear) + 1;
+                int daysExpected = (rental.DateIn.Value.DayOfYear - rental.DateOut.Value.DayOfYear);
+                if (daysExpected == 0) daysExpected = 1;
                 int? freeKms = daysExpected * price.FreeKMs;
                 //the following code will only get high season if the whole rental was within high season. this must be fixed
                 var season = (from sea in db.Seasons
@@ -170,6 +193,24 @@ namespace theking.Controllers
                 rental.SeasonHigh = season;
                 rental.Days = daysExpected;
                 db.SaveChanges();
+                //need customer name for sending personalized email:
+                Customer customer = db.Customers.Find(car.CustID);
+                string seasonString = season == true ? "high" : "low";
+                //first parameter should be customer.email, but since no domain set up yet, can't use other email addresses
+                SendEmail("eliandrivka@gmail.com", "Car Rented",
+                    $"Hi {customer.FirstName} {customer.LastName} \n \n" +
+                    $"Thank you for renting a car from The King Car! \n \n" +
+                    $"Details of your rental: \n" +
+                    $"{car.Color} {car.Make} {car.Model} \n" +
+                    $"License Plate Number: {car.LicensePlate} \n" +
+                    $"Class: ({car.Class}) {car.CarType} \n \n"+
+                    $"You have the car until: {rental.DateIn.Value.ToShortDateString()} \n" +
+                    $"The price per day in {seasonString} season is {rental.PriceDay.Value.ToString("F")}₪ \n" +
+                    $"You have {rental.FreeKMs} per day free KMs. Total free KMs: {freeKms} \n" +
+                    $"If you go over that total you will be charged the {seasonString} season price of {rental.PriceKM.Value.ToString("F")}₪ per extra KM \n \n" +
+                    "If you have any issues, please conact us \n \n"+
+                    "Yours Sincerely \n"+
+                    "The King Car");
                 return RedirectToAction("Details", new { id = rental.id });
             }
 
@@ -241,7 +282,8 @@ namespace theking.Controllers
                                select pri).FirstOrDefault();
                 int? kmsUsed = rental.MileageIn - rental.MileageOut;
                 //the following line will return the wrong info if car was returned different year to when it was taken
-                int daysUsed = (rental.DateIn.Value.DayOfYear - rental.DateOut.Value.DayOfYear) + 1;
+                int daysUsed = (rental.DateIn.Value.DayOfYear - rental.DateOut.Value.DayOfYear);
+                if (daysUsed ==0) daysUsed = 1;
                 int? freeKms = daysUsed * price.FreeKMs;
                 int? kmsToCharge = 0;
                 if (kmsUsed > freeKms)
@@ -271,6 +313,28 @@ namespace theking.Controllers
                 rental.SeasonHigh = season;
                 rental.Days = daysUsed;
                 db.SaveChanges();
+                //need customer name for sending personalized email:
+                Customer customer = db.Customers.Find(car.CustID);
+                string seasonString = season == true ? "high" : "low";
+                //first parameter should be customer.email, but since no domain set up yet, can't use other email addresses
+                SendEmail("eliandrivka@gmail.com", "Car Returned",
+                    $"Hi {customer.FirstName} {customer.LastName} \n \n" +
+                    $"Thank you for renting a car from The King Car! \n \n" +
+                    $"Details of your rental: \n" +
+                    $"{car.Color} {car.Make} {car.Model} \n" +
+                    $"License Plate Number: {car.LicensePlate} \n" +
+                    $"Class: ({car.Class}) {car.CarType} \n" +
+                    $"Mileage Out: {rental.MileageIn} KM \n" +
+                    $"Mileage In: {rental.MileageIn} KM \n \n" +
+                    $"You took the car on {rental.DateOut.Value.ToShortDateString()} and returned it on {rental.DateIn.Value.ToShortDateString()}  - {daysUsed} day(s). \n" +
+                    $"The price per day in {seasonString} season was {rental.PriceDay.Value.ToString("F")}₪ \n" +
+                    $"You had {rental.FreeKMs} free KMs per day. Total free KMs: {freeKms} \n" +
+                    $"The price per KM over that in {seasonString} season was {rental.PriceKM.Value.ToString("F")}₪ per KM \n" +
+                    $"You used {rental.KMsUsed} KMs" +
+                    $"The total you were charged was {rental.Price.Value.ToString("F")}₪ \n \n" +
+                    "If you have any issues, please conact us \n \n" +
+                    "Yours Sincerely \n" +
+                    "The King Car");
                 return RedirectToAction("Details", new {id=rental.id });
             }
             return View(rental);
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,102 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using theking.Models;
+
+namespace theking.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private CarRentalDBEntities db = new CarRentalDBEntities();
+             
+        [Route("api/getcustomer")]
+        // was like this: public IHttpActionResult GetCustomers(int id) and return statmenet was return Ok(customer); but it came as an array instead of single object
+        public Customer GetCustomer(int id)
+        {
+            var customer = db.Customers.Where(x => x.id == id);
+            return customer.FirstOrDefault();
+        }
+
+        [Route("api/getcustomers")]
+        public List<Customer> GetCustomers()
+        {
+            var customer = db.Customers;
+            return customer.ToList();
+        }
+
+        [HttpPost]
+        [Route("api/customer")]
+        public IHttpActionResult NewCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            db.Customers.Add(new Customer()
+            {
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                DOB = customer.DOB,
+                MemberSince = DateTime.Today,
+                Email = customer.Email,
+                IDnum = customer.IDnum,
+                Notes = "Customer added through WebAPI"
+            }); ;
+
+            db.SaveChanges();
+            return Ok();
+        }
+
+      
+        [HttpPost]
+        [Route("api/booking")]
+        public IHttpActionResult NewBooking(Booking booking)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            db.Bookings.Add(new Booking()
+            {
+                Class = booking.Class,
+                CustID = booking.CustID,
+                DateOut = booking.DateOut,
+                DateIn = booking.DateIn,
+                Notes = "this booking was made in WebAPI"
+            }); ;
+
+            db.SaveChanges();
+            return Ok();
+        }
+       // [HttpPut]
+        [Route("editcustomer")]
+        public IHttpActionResult Put(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+            
+                var existingCustomer = db.Customers.Where(s => s.id == customer.id)
+                                                        .FirstOrDefault<Customer>();
+
+                if (existingCustomer != null)
+                {
+                existingCustomer.FirstName = customer.FirstName;
+                existingCustomer.LastName = customer.LastName;
+                existingCustomer.IDnum = customer.IDnum;
+                existingCustomer.Email = customer.Email;
+                existingCustomer.Notes = "Customer edited through WebAPI";
+
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            
+
+            return Ok();
+        }
+
+
+    }
+}
